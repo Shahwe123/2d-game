c243049282ef455698f64f88a19f66cf3dd7aacf
Shahwe123 <100258324+Shahwe123@users.noreply.github.com>
2024-05-13 15:45:51 +1000

    - Class that represents anything that can be collected.

--- src/Classes/Collectibles/Collectible.js
+++ src/Classes/Collectibles/Collectible.js
@@ -1,7 +1,25 @@
+/**
+ *
+ * Collectibles - Represents anything that can be pick u[]
+ *
+ * @param position - an object with x and y
+ */
+
 export class Collectible {
-    constructor({position}) {
+    constructor({position, imageSrc, id}) {
         this.position = position
+        this.id = id
         this.isPickedUp = false
-        this.hasAppeared = false
+        this.image = new Image()
+        this.image.onload = () => {
+            this.height = this.image.height
+            this.width = this.image.width
+            this.loaded = true
+        }
+        this.image.src = imageSrc
+    }
+
+    draw({canvasContext}) {
+        canvasContext.drawImage(this.image, this.position.x, this.position.y)
     }
 }
