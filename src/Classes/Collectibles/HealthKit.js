153727a47e624007f6c286c92fb8a0748cd3e4a4
Shahwe123 <100258324+Shahwe123@users.noreply.github.com>
2024-05-13 15:46:53 +1000

    - Healthkit class, if picked heals the player.

--- src/Classes/Collectibles/HealthKit.js
+++ src/Classes/Collectibles/HealthKit.js
@@ -1,10 +1,19 @@
 import { Collectible } from "./Collectible";
+import healthBoxSrc from "../../assets/Collectibles/health.png"
 
 export class HealthKit extends Collectible {
-    constructor({position}) {
-        super({position})
+    constructor({position, mapKey, id}) {
+        super({position, imageSrc: healthBoxSrc, id})
         this.healingCapacity = 100
         this.width = 32
         this.height = 32
+        this.mapKey = mapKey
+    }
+
+    update({player}) {
+        player.currentHealth += 100
+        if (player.currentHealth > player.health) {
+            player.currentHealth = player.health
+        }
     }
 }
