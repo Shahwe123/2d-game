d9eed5aebe360f72592cc44229d02408e94c38b8
Shahwe123 <100258324+Shahwe123@users.noreply.github.com>
2024-05-19 00:08:05 +1000

    - Better documentation - three new functions for: 1. Update position of the players stamina bar 2. Player regenerates stamina gradually 3. Cooldown function if stamina reaches 0 - Code in attack() allows for attack animation randomisation and prevens attacking if stamina below 0

--- src/Classes/Player.js
+++ src/Classes/Player.js
@@ -15,9 +15,13 @@ export class Player extends Entity {
             y:1
         }
         this.type = "Player"
+
+        // animation variables
         this.currentFrame = 0
         this.elapsedFrames = 0
         this.frameBuffer = this.animations[this.currentSpriteKey].frameBuffer
+
+        // hitbox variables
         this.avatarHeight = 50
         this.avatarWidth = 30
         this.avatarPositionRight = {
@@ -29,6 +33,9 @@ export class Player extends Entity {
             y: 55
         }
         this.currentAvatarPosition = this.avatarPositionLeft
+
+
+        // combat variables
         this.attackBox = {
             position: {
                 x:0,
@@ -41,7 +48,14 @@ export class Player extends Entity {
         this.currentHealth = 300
         this.isDead = false
         this.isAttacking = false
-        this.attackPower = 10
+        this.attackPower = 15
+        this.stamina = 100
+        this.maxStamina = 100
+        this.attackStaminaCost = 20
+        this.cooldown = false
+        this.cooldownTime = 5000
+        this.regenerationRate = 10
+        this.nextAttackAnimation
     }
 
 
@@ -56,21 +70,29 @@ export class Player extends Entity {
      */
     update({canvasContext, currentMapCollisions, enemies, collectibles, currentMapKey}) {
 
-        this.attack({enemies, collectibles, currentMapKey})
+        // this.attack({enemies, collectibles, currentMapKey})
         this.updateFrames()
         this.draw({canvasContext})
         this.updateHitbox()
         this.updateHealthBarPosition()
+        this.updateStaminaBar()
         this.didCollectCollectables({collectibles})
         canvasContext.fillStyle = "red"
         canvasContext.fillRect(this.healthBar.position.x, this.healthBar.position.y, this.healthBar.width, this.healthBar.height)
-        canvasContext.fillStyle = "rgba(0,255,0,0.4)"
-        canvasContext.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+        canvasContext.fillStyle = "rgb(173,216,230)"
+        canvasContext.fillRect(this.staminaBar.position.x, this.staminaBar.position.y, this.staminaBar.width, this.staminaBar.height)
+
+
+        // canvasContext.fillStyle = "rgba(0,255,0,0.4)"
+        // canvasContext.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
+        this.regenerateStamina()
+
         this.draw({canvasContext})
         if (!this.isDead) this.position.x += this.velocity.x
         this.updateHitbox()
         this.updateAttackBox()
         this.updateHealthBarPosition()
+        this.updateStaminaBar()
         this.didCollectCollectables({collectibles})
         this.checkForHorizontalCollisions(currentMapCollisions)
 
@@ -78,6 +100,7 @@ export class Player extends Entity {
         this.updateHitbox()
         this.updateAttackBox()
         this.updateHealthBarPosition()
+        this.updateStaminaBar()
         this.didCollectCollectables({collectibles})
         this.checkForVerticalCollisions(currentMapCollisions)
     }
@@ -88,24 +111,41 @@ export class Player extends Entity {
      *  Player attacks enemy if there is a collision between the player and an enemy
      *
      * @param enemies - array of enemies in the current level
+     * @param collectibles - used to update the collectibles array with new drops
+     * @param currentMapKey - used to check if the enemy being attacked is in the current map
      *
      */
     attack({enemies, collectibles, currentMapKey}) {
-        if (this.isDead) {
+        // Prevents player from attacking
+        if (this.isDead || this.cooldown || this.stamina < this.attackStaminaCost) {
             return
         }
+
+        this.stamina -= this.attackStaminaCost
+
+        if (this.lastDirection === "left") {
+            this.nextAttackAnimation = this.selectRandomAttackAnimation(["attack1Left", "attack2Left", "attack3Left"])
+            this.switchSprite(this.nextAttackAnimation)
+        } else {
+            this.nextAttackAnimation = this.selectRandomAttackAnimation(["attack1", "attack2", "attack3"])
+            this.switchSprite(this.nextAttackAnimation)
+        }
+
+        if (this.stamina <= 0) {
+            this.startCooldown()
+        }
+
         enemies.forEach(enemy => {
             if (enemy.isDead) {
                 return
             }
-            //TODO: tryt to doing if ucrretnfame === check before and if true do collison check
-            if (enemy.currentMapKey === currentMapKey && this.isAttacking && this.currentFrame === this.animations['attack2'].frameRate - 1 &&  collison({entity: this.attackBox, block: enemy.hitbox}) ){
+            // TODO: still a little glitchy, sometimes does not respond
+            if (enemy.currentMapKey === currentMapKey && collison({entity: this.attackBox, block: enemy.hitbox}) ){
                 enemy.isHit = true
                 enemy.takeHit({player:this, collectibles})
                 this.currentFrame = 0
                 enemy.isHit = false
             }
-
         });
     }
 
@@ -134,8 +174,16 @@ export class Player extends Entity {
                 height:25
             }
         }
+        //TODO: maybe inside healthbar add acutal health to show progress
     }
 
+    /**
+     *
+     * Checks if there is a collision between the player and a collectible. If so,
+     * it is removed from the map and its effect is applied on the player
+     *
+     * @param {collectibles} param0
+     */
     didCollectCollectables({collectibles}) {
         collectibles.forEach(collectible => {
             if (collison({entity: this.hitbox, block: collectible}) && collectible.isPickedUp === false) {
@@ -144,5 +192,45 @@ export class Player extends Entity {
             }
         });
 
+    }
+
+    /**
+     * Updates the stamina bar ontop of the player, the width of the bar
+     * is reduced or increased depending on the player's current stamina
+     */
+    updateStaminaBar() {
+        this.originalStaminaBarWidth  = this.hitbox.width
+        this.staminaBar = {
+            position: {
+                x: this.hitbox.position.x,
+                y: this.hitbox.position.y - 18
+            },
+            width: (this.originalStaminaBarWidth / this.maxStamina) * this.stamina, // cuts the width based on the current stamina
+            height: 5
+        }
+    }
+
+    /**
+     * Gradually regenerates the player's stamina when the player is not attacking
+     */
+    regenerateStamina() {
+        if (!this.isAttacking && this.stamina <= this.maxStamina && !this.cooldown) {
+            this.stamina += this.regenerationRate * (1/60)
+            if (this.stamina > this.maxStamina) {
+                this.stamina = this.maxStamina
+            }
+        }
+    }
+
+    /**
+     * If the player's stamina reaches zero, starts a cool where player cannot attack until
+     * cooldown is finished, unlikely to run due to constant regeneration
+     */
+    startCooldown() {
+        this.cooldown = true
+        setTimeout(() => {
+            this.cooldown = false
+            this.stamina = this.maxStamina
+        }, this.cooldownTime);
     }
 }
