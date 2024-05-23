e103329fa2aa1bc1d8e0b51747f511fb55d09d42
Shahwe123 <100258324+Shahwe123@users.noreply.github.com>
2024-05-23 22:50:25 +1000

    - Enemy details constant, used potentially to rmove individual enemy classes, as they have almost the same functionality

--- src/Classes/Enemy/EnemyDetails.js
+++ src/Classes/Enemy/EnemyDetails.js
@@ -1,57 +1,98 @@
+import { evilwizardAnimations } from "../../assets/Enemies/EvilWizard/wizardImageExports";
+import { goblinAnimations } from "../../assets/Enemies/Goblin/imageExports";
+import { mushroomAnimations } from "../../assets/Enemies/Mushroom/imageExports";
+import { skeletonAnimations } from "../../assets/Enemies/Skeleton/imageExports";
+import { werewolfAnimations } from "../../assets/Enemies/White Werewolf/werewolfImageExports";
+
 export const enemyDetails = {
     EvilWizard: {
-        avatarHeight:0,
-        avatarWidth:0,
-        avatarPositionRight:{},
-        avatarPositionLeft:{},
-        health:0,
-        attackPower:0,
-        attackAnimationKeysRight: [],
-        attackAnimationKeysLeft:[],
-
+        type: "EvilWizard",
+        avatarHeight:95,
+        avatarWidth:48,
+        avatarPositionRight:{
+            x:110,
+            y: 70
+        },
+        avatarPositionLeft:{
+            x:110,
+            y: 70
+        },
+        health:300,
+        attackPower:35,
+        attackAnimationKeys: ["attack1", "attack2"],
+        attackAnimationKeysLeft:["attack1Left", "attack2Left"],
+        animations:evilwizardAnimations,
     },
     Goblin: {
-        avatarHeight:0,
-        avatarWidth:0,
-        avatarPositionRight:{},
-        avatarPositionLeft:{},
-        health:0,
-        attackPower:0,
-        attackAnimationKeysRight: [],
-        attackAnimationKeysLeft:[],
-
+        type: "Goblin",
+        avatarHeight:40,
+        avatarWidth:35,
+        avatarPositionRight:{
+            x:57,
+            y: 60
+        },
+        avatarPositionLeft:{
+            x:57,
+            y: 60
+        },
+        health:50,
+        attackPower:15,
+        attackAnimationKeys: ['attack'],
+        attackAnimationKeysLeft:['attackLeft'],
+        animations:goblinAnimations,
     },
     Mushroom: {
-        avatarHeight:0,
-        avatarWidth:0,
-        avatarPositionRight:{},
-        avatarPositionLeft:{},
-        health:0,
-        attackPower:0,
-        attackAnimationKeysRight: [],
-        attackAnimationKeysLeft:[],
-
+        type: "Mushroom",
+        avatarHeight:40,
+        avatarWidth:35,
+        avatarPositionRight:{
+            x:57,
+            y: 60
+        },
+        avatarPositionLeft:{
+            x:57,
+            y: 60
+        },
+        health:100,
+        attackPower:35,
+        attackAnimationKeys: ['attack'],
+        attackAnimationKeysLeft:['attackLeft'],
+        animations:mushroomAnimations,
     },
     Skeleton: {
-        avatarHeight:0,
-        avatarWidth:0,
-        avatarPositionRight:{},
-        avatarPositionLeft:{},
-        health:0,
-        attackPower:0,
-        attackAnimationKeysRight: [],
-        attackAnimationKeysLeft:[],
-
+        type: "Skeleton",
+        avatarHeight:50,
+        avatarWidth:48,
+        avatarPositionRight:{
+            x:57,
+            y: 50
+        },
+        avatarPositionLeft:{
+            x:45,
+            y: 50
+        },
+        health:50,
+        attackPower:45,
+        attackAnimationKeys: ['attack'],
+        attackAnimationKeysLeft:['attackLeft'],
+        animations:skeletonAnimations,
     },
     WhiteWerewolf: {
-        avatarHeight:0,
-        avatarWidth:0,
-        avatarPositionRight:{},
-        avatarPositionLeft:{},
-        health:0,
-        attackPower:0,
-        attackAnimationKeysRight: [],
-        attackAnimationKeysLeft:[],
-
+        type: "WhiteWerewolf",
+        avatarHeight:72,
+        avatarWidth:82,
+        avatarPositionRight:{
+            x:20,
+            y: 55
+        },
+        avatarPositionLeft:{
+            x:20,
+            y: 55
+        },
+        health:350,
+        attackPower:50,
+        attackAnimationKeys: ["attack1", "attack2", "attack3", "runAttackRight"],
+        attackAnimationKeysLeft:["attack1Left", "attack2Left", "attack3Left", "runAttackLeft"],
+        animations:werewolfAnimations,
     }
 }
