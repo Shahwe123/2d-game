be55485ad8132177f8a5dfed6410b7d7cbd039ac
Shahwe123 <100258324+Shahwe123@users.noreply.github.com>
2024-05-26 00:03:35 +1000

    - new enemy

---
+++ src/Classes/Enemy/FlyingEye.js
@@ -0,0 +1,94 @@
+import { collison } from "../../utils"
+import { Entity } from "../Entity"
+import { Enemy } from "./Enemy"
+import { flyingEyeAnimations } from "../../assets/Enemies/FlyingEye/imageExports"
+/**
+ *
+ *  Goblin - represents an enemy entity
+ *
+ * @param position - an object of x and y
+ * @param currentMapKey - which map the enemy is located in
+ * @param roamingPosition - x and y object of where the enemy loops between
+ *
+ */
+
+export class FlyingEye extends Enemy {
+    constructor({position, currentMapKey, roamingPosition}) {
+        super({position: position, animations: flyingEyeAnimations})
+        this.currentMapKey = currentMapKey
+        this.type = "FlyingEye"
+        this.roamingPosition = roamingPosition
+        this.velocity = {
+            x:0,
+            y:1
+        }
+        this.avatarHeight = 40
+        this.avatarWidth = 35
+        this.avatarPositionRight = {
+            x:57,
+            y: 60
+        }
+        this.avatarPositionLeft = {
+            x:57,
+            y: 60
+        }
+        this.currentAvatarPosition = this.avatarPositionLeft
+        this.currentFrame = 0
+        this.elapsedFrames = 0
+        this.frameBuffer = this.animations[this.currentSpriteKey].frameBuffer
+
+        // combat code
+        this.alerted = false
+        this.roamDirection = "left"
+        this.attackBox = {
+            position: {
+                x:0,
+                y:0
+            },
+            width:70,
+            height:25
+        }
+
+
+        this.health = 50
+        this.currentHealth = 50
+        this.attackPower = 15
+        this.attackAnimationKeys = ['attack']
+        this.attackAnimationKeysLeft = ['attackLeft']
+    }
+
+    /**
+     * As the entity moves, its detection area also updates.
+     */
+    updateDetectionArea() {
+        this.detectionArea = {
+            position:{
+                x:this.hitbox.position.x - 125,
+                y:this.hitbox.position.y
+            },
+            width: 300,
+            height:this.avatarHeight
+        }
+    }
+    /**
+     * As the entity moves, its attackbox both left and right updates
+     */
+    updateAttackBox() {
+        this.attackBoxLeft = {
+            position: {
+                x:this.hitbox.position.x - 25,
+                y:this.hitbox.position.y
+            },
+            width:45,
+            height:25
+        }
+        this.attackBoxRight = {
+            position: {
+                x:this.hitbox.position.x + 10,
+                y:this.hitbox.position.y
+            },
+            width:56,
+            height:25
+        }
+    }
+}
