8790d518ca47b43f0e86e7a001f3842df64b41a0
Shahwe123 <100258324+Shahwe123@users.noreply.github.com>
2024-05-25 23:55:54 +1000

    -  Removed this for coincollected

--- src/Classes/Objectives/ItemsCollected.js
+++
@@ -1,8 +0,0 @@
-import { Objective } from "./Objective"
-
-export class ItemsCollected extends Objective {
-    constructor({noItemsToCollect}) {
-        super()
-        this.type = "itemsCollected"
-    }
-}
